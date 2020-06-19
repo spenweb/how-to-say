85b70cea07767ea9577ef4d2c5f9dbfa2066ec0b
Spencer Brown <git@spencerwebservices.com>
2020-06-19 11:06:05 -0700

    Saves history to user's directory

--- howtosay.py
+++ howtosay.py
@@ -4,27 +4,44 @@ import pygame
 from io import BytesIO
 import subprocess
 import time
+from pathlib import Path
+import os
+
+
+database_file_name = ".howtosayDB.txt"
+
 
 pygame.init()
 
-""" Based off of https://github.com/pndurette/gTTS/issues/26#issuecomment-552242214
-"""
+
 def say(text):
-  tts = gTTS(text=text, lang='en')
-  fp = BytesIO()
-  tts.write_to_fp(fp)
-  fp.seek(0)
-  pygame.mixer.init()
-  pygame.mixer.music.load(fp)
-  time.sleep(1)
-  pygame.mixer.music.play()
-  while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
+    """ Based off of https://github.com/pndurette/gTTS/issues/26#issuecomment-552242214
+    """
+    tts = gTTS(text=text, lang="en") #TODO: Support language detection
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    pygame.mixer.init()
+    pygame.mixer.music.load(fp)
+    time.sleep(1)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+
+def save_to_database(clipboard: str, filename: str):
+    home = str(Path.home())
+    filename_with_path = os.path.join(home, filename)
+    with open(filename_with_path, 'a+') as f:
+        f.write(clipboard + "\n")
+
 
 def main():
-  clipboard = subprocess.check_output(["pbpaste"], text=True)
-  print(clipboard)
-  say("how to say " + clipboard)
+    clipboard = subprocess.check_output(["pbpaste"], text=True)
+    print(clipboard)
+    save_to_database(clipboard, database_file_name)
+    say("how to say " + clipboard)
+
 
 if __name__ == "__main__":
-  main()
+    main()
